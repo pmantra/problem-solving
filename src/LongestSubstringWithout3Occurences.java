9716d8ba84c49e808baabcaf697b06206c106bbd
prashanth <prashanth.manravadi@gmail.com>
2020-02-13 22:33:17 -0500

    longest substring without 3 contiguous occurences of a letter

---
+++ src/LongestSubstringWithout3Occurences.java
@@ -0,0 +1,35 @@
+public class LongestSubstringWithout3Occurences {
+
+  public static void main(String...strings) {
+    String s = "aabbaaaaabb";
+    LongestSubstringWithout3Occurences sol = new LongestSubstringWithout3Occurences();
+    System.out.println(sol.getLongestSubString(s));
+  }
+
+  public String getLongestSubString(String s) {
+    int max = Integer.MIN_VALUE;
+    int curr = 0, start = 0, end = 1;
+    int count = 0;
+    String retVal = "";
+    while (end < s.length()) {
+      if(s.charAt(curr) == s.charAt(end)) {
+        count++;
+        if(count == 2) {
+          if(end-start > max) {
+            max = end-start;
+            retVal = s.substring(start, end);
+          }
+          start = curr;
+        }
+      }else {
+        count=0;
+      }
+      ++end;
+      ++curr;
+    }
+    if(end-start > max) {
+      retVal = s.substring(start, end);
+    }
+    return retVal;
+  }
+}
