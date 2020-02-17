54213da63522cff5de411c3c44c945a55480856f
prashanth <prashanth.manravadi@gmail.com>
2020-02-17 08:09:32 -0500

    contains duplicate leetcode

---
+++ src/ContainsDuplicateII.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    if(nums.length == 0) return false;
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i=0; i<nums.length; i++) {
+      if(map.containsKey(nums[i])) {
+        if(i-map.get(nums[i]) <= k) return true;
+      }
+      map.put(nums[i], i);
+    }
+    return false;
+  }
+}
